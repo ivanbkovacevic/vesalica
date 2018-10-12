6f405f32f1372467f9019e6fc2e3c5f90103427b
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-10-12 17:47:43 +0200

    uradjene neke pocetne stvari

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Crtica from './Crtica'
 class App extends Component {
+
+state={
+  zagRec:'burek',
+  showWord:false,
+  zagRecLength:0,
+  zagRecArr:[],
+  letterG:''
+}
+
+generateWord=()=>{
+  let {zagRec,zagRecLength,zagRecArr}=this.state;
+   zagRecLength= zagRec.length;
+   zagRecArr= Array.from(zagRec);
+
+   this.setState({zagRec,zagRecLength,zagRecArr});
+}
+
+showWord=()=>{
+  let {showWord}=this.state;
+  showWord= true;
+   this.setState({showWord});
+}
+
+GuessingLetter=(e)=>{
+  e.preventDefault();
+ 
+let letterG = this.refs.letter.value;
+console.log(letterG);
+this.setState({letterG})
+
+}
   render() {
+   let crtice=null;
+      crtice =this.state.zagRecArr.map((slovo,i)=>{
+       return <Crtica o={slovo}/>
+     })
+
     return (
       <div className="App">
       TEST VESALICA
+      <button onClick={this.generateWord}>izaberi rec</button>
+      <button onClick={this.showWord}>prikazi rec</button>
+      <div>{this.state.zagRec}</div>
+      <div>{this.state.zagRecLength}</div>
+      {this.state.showWord ? <div>{this.state.zagRec}</div> : null} 
+      {crtice}
+
+      <form onSubmit={this.GuessingLetter}>
+      <input type='text' placeholder='pogodi slovo' ref="letter"></input>
+      <button type='submit'>da li ima ovog slova</button>
+      </form>
+      {this.state.letterG}
       </div>
     );
   }
---
+++ src/Crtica.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Crtica extends Component {
+    render() {
+        return (
+            <span> _ </span>
+        );
+    }
+}
+
+export default Crtica;
