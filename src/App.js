94726310212ad5a6e1f607932607fa3f0778522e
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-10-15 01:49:39 +0200

    uradjeno dosta...sredjen cikica...sredjeno sa raymacima ...ostalo jos da ne mogu vise puta ista slova da se pisu

--- src/App.js
+++ src/App.js
@@ -6,27 +6,39 @@ import { Grid, Col, Row,Button } from 'react-bootstrap';
 class App extends Component {
 
 state={
-  zagRec:'avatar',
+  zagRec:'novi sad',
   showWord:false,
   zagRecLength:0,
   zagRecArr:[],
+  zagRecArrChecking:[],
   correctLettArr:[],
   bingo:0,
   missed:0,
   letterG:'',
-  atmLettArr:[],
+  atmLettArr:['x'],
   notMatch:0,
-  message:''
+  message:'',
+  status:''
 }
 
-generateWord=()=>{ // kreira rec...i kreira crtice za slova 
-  let {zagRec,zagRecLength,zagRecArr,correctLettArr,notMatch}=this.state;
-   zagRecLength= zagRec.length;
-   zagRecArr= Array.from(zagRec);
-   for(let c in zagRecArr){
-     correctLettArr.push('_ ')
+generateWord=()=>{ // kreira rec...i kreira crtice za slova i pravi nekoliko arraya
+  let {zagRec,zagRecLength,zagRecArr,correctLettArr,notMatch,zagRecArrChecking}=this.state;
+   zagRecLength= zagRec.length;//du\ina nam treba da bismo proveravali da li je doslo do pobede ili poraza kao i za crtanje
+   zagRec=zagRec.toUpperCase();
+   zagRecArr=  Array.from(zagRec);// pravljenje array od reci jer treba manipulisati sa tim array
+   zagRecArrChecking=[...zagRecArr];//array koji sluzi za proveru da li su sva slova pogodjena tj.  registruje da li su sva slova(i duplikati) upisani
+   
+   for(let c in zagRecArr){ // loop koji proverava da li ima razmaka tj da li ima dve ili vise reci i izbacuje taj elemenat i smanjuje duzinu
+     if(zagRecArr[c]===' '){// ovo mora jer inace ubacio bi ubacio crticu i na prayno mesto
+      correctLettArr.push(' ');
+      zagRecArrChecking.splice(c,1);
+      zagRecLength--;
+     }else{
+      correctLettArr.push('_')
+     }
+    
    }
-   this.setState({zagRec,zagRecLength,zagRecArr,correctLettArr,notMatch});
+   this.setState({zagRec,zagRecLength,zagRecArr,correctLettArr,notMatch,zagRecArrChecking});
 }
 
 showWord=()=>{ //prikazuje zagonetnu rec...to je kada igrac ne moze da pronadje rec
@@ -37,99 +49,144 @@ showWord=()=>{ //prikazuje zagonetnu rec...to je kada igrac ne moze da pronadje 
 
 
 GuessingLetter=(e)=>{ // igrac pogadja rec...u input upisuje slova i submituje ih  
-  //...ovde treba da se cekira da li ima slova u zagReci kao i ubacivati pokusana slova u neki array koji ce ih prikazivati 
    e.preventDefault();
-  let {letterG,atmLettArr,zagRecArr,message,correctLettArr,bingo,zagRecLength,notMatch,zagRec}=this.state;
+  let {letterG,atmLettArr,zagRecArr,zagRecArrChecking,message,correctLettArr,bingo,zagRecLength,notMatch,missed,status}=this.state;
 
-  atmLettArr=atmLettArr.slice();
+  atmLettArr=atmLettArr.slice();//niz koji ispisuje slova koje je igrac sve pokusao
   zagRecArr=zagRecArr.slice();
-  correctLettArr=correctLettArr.slice();
-    letterG = this.refs.letter.value;
-    atmLettArr.push(letterG);
+  zagRecArrChecking=zagRecArrChecking.slice();
+  correctLettArr=correctLettArr.slice();//array koji hvata tacna slova  tj pogotke
+
+    letterG = this.refs.letter.value;//hvatanje slova iz inputa
+    letterG=letterG.toUpperCase();
+    
+    
+    for(let q in atmLettArr){///////////ovo treba da se sredi/////////////
+       if(letterG === atmLettArr[q]){
+        message='Vec ste probali ovo slovo';
+        this.setState({message});
+       }else{
+        atmLettArr.push(letterG);
+        this.setState({atmLettArr});
+       }
+    }
+   
 
     for(let w in zagRecArr ){ //prolazenje kroz arr reci i cekiranje da li se neki elemenat arr poklapa sa guessovanim slovom
       if(zagRecArr[w]===letterG){
-        correctLettArr[w]=letterG;
-        bingo++;
-        this.setState({correctLettArr,bingo});
-        }else{
-        notMatch++;
-        message='nema tog slova u reci';
-        this.setState({message,notMatch});
-        }   
+        correctLettArr[w]=letterG;//correctLettArr sluzi za ispisivanje pogodjenih slova....mo\da je moglo da se nayove i displayedLettArr
+        message='Pogodili ste slovo';
+        this.setState({correctLettArr,message});
+      } 
   }
 
-      if(bingo===zagRecLength){ //pronaslo se tacno resenje
+  for(let ww in zagRecArrChecking ){ //cekiranje pogodatak i dobijanje da li pobeda ili ne
+    if(zagRecArrChecking[ww]===letterG){
+      zagRecArrChecking.splice(ww,1); // zagRecArrChecking sluzi za proveru da li je pronadjena zagonetna rec...taj array se smanjuje...izbacuju se pogodjena slova i dodaje se po poen u bingo 
+      bingo++;
+      this.setState({zagRecArrChecking,bingo});
+    } 
+}
+
+  this.checkSlovo=(slovo)=>{ // funkcija koja vraca true ili false u zavisnsoti od uslova
+    return slovo !== letterG;
+   }
+
+   notMatch=zagRecArr.every(this.checkSlovo);//primenjivanje gornje funkcije na svaki elemenat u nizu
+   
+     if(notMatch){//ako je true dodavanje poena u missed  i ispisavanje poruke
+       message='Nema tog slova u reci !';
+       missed++
+       this.setState({message,missed})
+     }
+
+      if(bingo===zagRecLength){ //pronaslo se tacno resenje 
         message='BRAVO! Nasli ste tacnu rec.';
-        this.setState({message})
+        status='-win';
+        this.setState({message,status})
       }
-      if(atmLettArr.length-bingo>4 ){ //ako je broj pokusaja veci od pogodaka i ako je broj pokusaja veci od 5
+
+      if(missed===6 ){ //missed se dobija kada slovo nije matchovano
         message='IZGUBILI STE PARTIJU';
-        this.setState({message})
+        status='-loss';
+        this.setState({message,status})
       }
-    this.setState({letterG,atmLettArr})
+
+    this.setState({letterG})
     e.target.reset();
 }
 
 newGame=()=>{
-  let{zagRec,zagRecLength,zagRecArr,correctLettArr,showWord,bingo,letterG,atmLettArr,message}=this.state;
+  
   this.setState({
-  zagRec:'avatar',
+  zagRec:'novi sad',
   showWord:false,
   zagRecLength:0,
   zagRecArr:[],
+  zagRecArrChecking:[],
   correctLettArr:[],
   bingo:0,
+  missed:0,
   letterG:'',
   atmLettArr:[],
-  message:''
+  message:'',
+  status:''
 });
 
 }
 
   render() {
-    let {letterG,atmLettArr,zagRecArr,message,correctLettArr}=this.state;
+    let {status,correctLettArr}=this.state;
   
    let correct=null;
    let guessedLett=null;
    
      guessedLett = this.state.atmLettArr.map((gl,i)=>{
-      return <span> {gl} ,</span>
+      return <span className='crtice'> {gl} ,</span>
     })
 
     correct=correctLettArr.map((l,i)=>{
-      return <span>{l}</span>
+      return <span className='crtice'>{l}</span>
     })
 
     return (
       <Grid >
         <Row>
-        <div>
-          <button className="btnmoj" onClick={this.generateWord}>izaberi rec</button>
-          <button className="btnmoj" onClick={this.showWord}>prikazi rec</button>
-          <button className="btnmoj" onClick={this.newGame}>nova igra</button>
-          
-        </div>
+          <Col xs={4}><button className="btnmoj" onClick={this.generateWord}>izaberi rec</button></Col>
+          <Col xs={4}> <button className="btnmoj" onClick={this.showWord}>prikazi rec</button></Col>
+          <Col xs={4}>  <button className="btnmoj" onClick={this.newGame}>nova igra</button></Col>
         </Row>
+
         <Row>
-          {this.state.showWord ? <div className="letters">{this.state.zagRec}</div> : null} 
-          <span className="letters">{correct}</span>
+          <div className='zag-rec-container'>
+              {this.state.showWord ? <div className="letters">{this.state.zagRec}</div> : null} 
+              <Col xs={12}><span className="letters-zagonetka">{correct}</span></Col>
+          </div>
+  
         </Row>
     <Row>
-    <form onSubmit={this.GuessingLetter}>
-      <input type='text' placeholder='pogodi slovo' ref="letter" ></input>
+      <Col xs={12}> 
+      <form onSubmit={this.GuessingLetter}>
+      <input type='text' maxLength="1" size="1" ref="letter" ></input>
       <button className="btnmoj" type='submit'>Da li ima ovog slova?</button>
       </form>
+      </Col>
+   
     </Row>
     <Row>
-      <span className="letters">Pokušana slova:  {guessedLett}</span>
-       <br/>
+    <Col xs={12}> 
+    <div> <span className="letters">Pokušana slova:  {guessedLett}</span></div> 
+    </Col>
+     
     </Row>
     <Row>
-    <span className="letters">{this.state.message}</span>
+    <Col xs={12}> 
+       <div className={`message${status}`}>{this.state.message}</div>
+    </Col>
+  
     </Row>
     <Row>
-      <Vesalo />
+      <Vesalo missed={this.state.missed}/>
     </Row>
 
       </Grid>
--- src/Vesalo.js
+++ src/Vesalo.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 
 class Vesalo extends Component {
+
     render() {
         return (
-            <div>
-              <div className='vesalo-container'>
-               <div className='vesalo-glava'>glava</div>
-               <div className='vesalo-torzo'>torzo</div>
-               <div className='vesalo-Lruka'>L ruka</div>
-               <div className='vesalo-Druka'>D ruka</div>
-               <div className='vesalo-Lnoga'>L noga</div>
-               <div className='vesalo-Dnoga'>D noga</div>
+            <div className='wraper-ceo'>
+              <div className='wraper'>
+              <div >
+               {this.props.missed >0 ? <div className='vesalo-glava'>-</div> : null}
+               {this.props.missed >1 ? <div className='vesalo-torzo'>-</div>: null}
+               {this.props.missed >2 ? <div className='vesalo-Lruka'>-</div>: null}
+                {this.props.missed >3 ? <div className='vesalo-Druka'>-</div>: null}
+                {this.props.missed >4 ? <div className='vesalo-Lnoga'>-</div>: null}
+                {this.props.missed >5 ? <div className='vesalo-Dnoga'>-</div>: null}
              </div>
             </div>
+            </div>
+            
        
         );
     }
