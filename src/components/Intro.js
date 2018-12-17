b94e3547313df1ac652abad9db6c7b81ca248c0a
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-12-17 17:28:39 +0100

    vfejkn

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,10 +11,10 @@ class Intro extends Component {
     render() {       
         return (
             <div id="introContainer" className="intro-container">
-                <h1>Dobro došli u igricu Vešalica</h1>
-                 <p>Cilj igre je da pogodite zagonetnu rec</p>
+                <h1>Dobro došli u igricu - Vešalica</h1>
+                 <p>Cilj igre je da pogodite zagonetnu reč</p>
                  <p>U pitanju su gradovi Srbije</p>
-                 <button className="btnMy btnMy-intro" onClick={this.introSlide}>Pocni sa igrom</button>
+                 <button className="btnMy btnMy-intro" onClick={this.introSlide}>Počni sa igrom</button>
             </div>
         );
     }
