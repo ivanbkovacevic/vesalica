2da3d2653733f20b1ecd21114537e1a7d80c99d8
ivanbkovacevic <ivanbkovacevic@gmail.com>
2019-01-13 23:35:51 +0100

    facebook login inserted

---
+++ src/components/PrivacyPolisy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PrivacyPolisy extends Component {
+    render() {
+        return (
+            <div>
+                It is ok to use this app
+            </div>
+        );
+    }
+}
+
+export default PrivacyPolisy;
